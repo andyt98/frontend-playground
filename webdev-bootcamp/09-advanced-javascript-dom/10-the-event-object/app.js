374f5b1f8868917da65808de98989898a5a1c2c7
Andy Toma <andytoma@Andys-Mac-mini.local>
2024-06-03 20:37:13 +0300

    webdev bootcamp

--- webdev-bootcamp/09-advanced-javascript-dom/10-the-event-object/app.js
+++ webdev-bootcamp/09-advanced-javascript-dom/10-the-event-object/app.js
@@ -1,21 +1,20 @@
-let paragraphElement = document.querySelector('p');
+let paragraphElement = document.querySelector("p");
 
 function changeParagraphText(event) {
-  paragraphElement.textContent = 'Clicked!';
-  console.log('Paragraph clicked!');
-  console.log(event);
+  paragraphElement.textContent = "Clicked!";
+  console.log("Paragraph clicked!");
+  console.log("Event " + event);
 }
 
-paragraphElement.addEventListener('click', changeParagraphText);
+paragraphElement.addEventListener("click", changeParagraphText);
 
-let inputElement = document.querySelector('input');
+let inputElement = document.querySelector("input");
 
 function retrieveUserInput(event) {
   // let enteredText = inputElement.value;
-  let enteredText = event.target.value;
-  // let enteredText = event.data; => This is different!
-  console.log(enteredText);
-  // console.log(event);
+  console.log("Event " + event);
+  console.log(event.data);
+  console.log(event.target.value);
 }
 
-inputElement.addEventListener('input', retrieveUserInput);
+inputElement.addEventListener("input", retrieveUserInput);
